8b09cfb9a32cb0f54714c95b8753e4721b1a19d5
yali30814936 <73872860+yali30814936@users.noreply.github.com>
2021-05-31 00:20:42 +0800

    facebook small improve

---
+++ TestModules/src/test/java/KoInMaster/TestModules/FB/DataGet.java
@@ -0,0 +1,43 @@
+package KoInMaster.TestModules.FB;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.IOException;
+
+
+public class DataGet {
+    private Document doc;
+    private Elements postitems;
+    public  void setElement() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver","D:\\Programing\\Java\\final_project\\KoInMaster\\TestModules\\chromedriver.exe");
+        WebDriver browser =new ChromeDriver();
+        browser.get("https://zh-tw.facebook.com/ey.gov.tw/");
+       for(int i=0;i<5;i++) {
+            Thread.sleep(5000);
+           ((JavascriptExecutor) browser).executeScript("window.scrollTo(0,document.body.scrollHeight)");
+        }
+        browser.findElement(By.id("expanding_cta_close_button")).click();
+        Thread.sleep(5000);
+        doc = Jsoup.parse(browser.getPageSource());
+        postitems=doc.getElementsByClass("_1dwg _1w_m _q7o");
+
+
+        for(Element postitem :postitems){
+            Elements content=postitem.getElementsByClass("_5pbx userContent _3576");
+            Elements image=postitem.getElementsByClass("uiScaledImageContainer");
+            Elements posthref=postitem.getElementsByClass("_5pcq");
+            System.out.printf("%s%n",image.attr("src"));
+            System.out.printf("%s%n",content.text());
+            System.out.printf("%s%n",posthref.attr("href"));
+            System.out.println("----------------");
+        }
+    }
+
+}
