10f49523ef71b0a6519bebca7d579a9b3e4e5300
yali30814936 <73872860+yali30814936@users.noreply.github.com>
2021-05-31 09:37:22 +0800

    twitter-complete

--- TestModules/src/test/java/KoInMaster/TestModules/Posts/TwitterPost.java
+++ TestModules/src/test/java/KoInMaster/TestModules/Posts/TwitterPost.java
@@ -2,17 +2,21 @@ package KoInMaster.TestModules.Posts;
 
 import twitter4j.MediaEntity;
 import twitter4j.Status;
+
+import java.util.ArrayList;
+
 public class TwitterPost extends Post{
     public TwitterPost(Status status){
         text="@"+status.getUser().getScreenName()+"\n"+status.getText();
         if(status.isRetweet()){
-            type="RT";
+            type="twitter-RT";
         }
         else{
-            type="T";
+            type="twitter-T";
         }
         url = "https://twitter.com/"+status.getUser().getScreenName()+"status"+status.getId();
         publishedTime=status.getCreatedAt();
+        media = new ArrayList<>();
         for(MediaEntity me:status.getMediaEntities()){
             media.add(me.getMediaURL());
         }
