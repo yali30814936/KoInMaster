6192c5fc744d7c1073ff7301d408954f5750b7fc
yali30814936 <73872860+yali30814936@users.noreply.github.com>
2021-06-11 14:02:30 +0800

    GUI貼文元件

--- TestModules/src/test/java/KoInMaster/TestModules/Celebrities/Crawlers/kindatest.java
+++ TestModules/src/test/java/KoInMaster/TestModules/Celebrities/Crawlers/kindatest.java
@@ -3,11 +3,10 @@ package KoInMaster.TestModules.Celebrities.Crawlers;
 import KoInMaster.TestModules.Posts.Post;
 
 import java.util.List;
-import java.util.concurrent.Callable;
 
 public class kindatest {
 	public static void main(String[] args) throws Exception {
-		Crawler foo = new YoutubeCrawler("DOLLARplays","DOLLARplays");
+		Crawler foo = new YoutubeCrawler("雪花ラミィ","UCFKOVgVbGmX65RxO3EtH3iw");
 		System.out.println("ready to crawl");
 		List<Post> list = foo.call();
 		for (Post p:list)
---
+++ TestModules/src/test/java/KoInMaster/TestModules/GUI/Block.java
@@ -0,0 +1,11 @@
+package KoInMaster.TestModules.GUI;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Block extends JPanel {
+	public Block() {
+		super(new GridLayout(3,1));
+
+	}
+}
