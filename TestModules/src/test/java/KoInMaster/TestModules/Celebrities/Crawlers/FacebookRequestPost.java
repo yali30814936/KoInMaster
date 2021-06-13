887dc53bcd5739521f5dd9f95b38672d5060f888
yali30814936 <73872860+yali30814936@users.noreply.github.com>
2021-06-13 18:08:17 +0800

    Update FacebookRequestPost.java

--- TestModules/src/test/java/KoInMaster/TestModules/Celebrities/Crawlers/FacebookRequestPost.java
+++ TestModules/src/test/java/KoInMaster/TestModules/Celebrities/Crawlers/FacebookRequestPost.java
@@ -15,11 +15,11 @@ import java.net.URISyntaxException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class FacebookCrawlerV3 {
+public class FacebookRequestPost {
 	private final CloseableHttpClient http;
 	private final HttpGet request;
 
-	public FacebookCrawlerV3(String pageId) throws URISyntaxException {
+	public FacebookRequestPost(String pageId) throws URISyntaxException {
 		http = HttpClients.createDefault();
 
 		URI uri = new URIBuilder("https://www.facebook.com/pages_reaction_units/more/?surface=www_pages_home&referrer&privacy_mutation_token=eyJ0eXBlIjowLCJjcmVhdGlvbl90aW1lIjoxNjIzNDE2NDc3LCJjYWxsc2l0ZV9pZCI6NjgzMjQxNzc1NzExMjE3fQ%3D%3D&fb_dtsg_ag&__user=0&__a=1&__dyn=7AgNe5Gmawgrolg5K8G6EjheC1szobpEnz8nwgU5GexZ3ocWwAyUuKewhE4mdwJx64e2q3qcw8258e8hwj82oG3i0wpk2u2-263WWwSxu15wgE46fw9C48szU2mwwwg8vy8465o-cypojwp87612wOKi8wGwFyE6K3a1txm2l2UtG7o4y2O0B85WEeE5m1mzUuxe6Fo5e4Ueo2swkEbElxm3y1lxu16wa-0kS1AyES&__csr=&__req=6&__hs=18789.PHASED%3ADEFAULT.2.0.0.0&dpr=1.5&__ccg=EXCELLENT&__rev=1003951477&__s=bv9qk2%3Aql2gni%3Aaom84m&__hsi=6972520678623437500-0&__comet_req=0&__spin_r=1003951477&__spin_b=trunk&__spin_t=1623416477")
@@ -49,7 +49,7 @@ public class FacebookCrawlerV3 {
 	}
 
 	public static void main(String[] args) throws IOException, URISyntaxException {
-		FacebookCrawlerV3 v3 = new FacebookCrawlerV3("1421110321440678");
+		FacebookRequestPost v3 = new FacebookRequestPost("1421110321440678");
 		System.out.println(v3.getResponse());
 	}
 }
