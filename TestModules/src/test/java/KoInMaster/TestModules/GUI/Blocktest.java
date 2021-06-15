d83222d3f0f939e32bec17c309e0bd0258248d37
Xuemao1018 <84667037+Xuemao1018@users.noreply.github.com>
2021-06-15 22:42:12 +0800

    block image ver.1
    perform only one image

--- TestModules/src/test/java/KoInMaster/TestModules/GUI/Blocktest.java
+++ TestModules/src/test/java/KoInMaster/TestModules/GUI/Blocktest.java
@@ -9,9 +9,9 @@ public class Blocktest {
     private static TwitterCrawler Tweets;
     public static void main(String[] args) throws Exception {
         JFrame frame = new JFrame();
-        Tweets = new TwitterCrawler("usadapekora","usadapekora");
+        Tweets = new TwitterCrawler("minatoaqua","minatoaqua");
         PostList list = Tweets.call();
-        Block bpanel = new Block(list.get(19));
+        Block bpanel = new Block(list.get(9));
         JScrollPane scrollPane = new JScrollPane(bpanel);
         frame.add(scrollPane);
         frame.setSize(1000, 1000);
